fix: fixed calculation of covariance matrix

end2endModel.py
@@ -36,7 +36,6 @@ class end2endModel(tf.keras.Model):
         
         self.demapper = sn.mapping.Demapper("app", constellation=self.constellation, num_bits_per_symbol=self.num_bits_per_symbol)
     
-    @tf.function
     def __call__(self, batch_size, ebno_db):
         
         #pilot phase
@@ -52,18 +51,16 @@ class end2endModel(tf.keras.Model):
         
         # print('shape of x: ', x.shape)
                                 
-        pilot = tf.ones((batch_size, 1, 1), dtype=tf.complex64)
+        pilot = tf.ones([batch_size,1,1], dtype=tf.complex64)
                         
         y_p, h, C = self.channel(pilot, no, batch_size, self.n_coherence, self.n_antennas)
               
         h_hat_ls = self.ls_estimator(y_p, pilot)
 
         h_hat_mmse = self.mmse_estimator(y_p, no, C, pilot)
-        
-        
-                
-        # print('difference between h_hat_ls and h: ', tf.reduce_sum(tf.abs(h_hat_ls - h)))
-        # print('difference between h_hat_mmse and h: ', tf.reduce_sum(tf.abs(h_hat_mmse - tf.cast(h, dtype=tf.complex64))))
+                        
+        print('difference between h_hat_ls and h: ', tf.reduce_sum(tf.square(tf.abs(h_hat_ls - tf.cast(h, dtype=tf.complex64)))) / tf.reduce_sum(tf.square(tf.abs(h))))
+        print('difference between h_hat_mmse and h: ', tf.reduce_sum(tf.square(tf.abs(h_hat_mmse - tf.cast(h, dtype=tf.complex64)))) / tf.reduce_sum(tf.square(tf.abs(h))))
                                 
         #uplink phase
                 
@@ -87,7 +84,7 @@ class end2endModel(tf.keras.Model):
             # print('y_i.shape: ', y_i.shape)
             y.append(y_i)
         
-            x_hat_ls_i, no_ls_new_i = self.equalizer(h_hat_ls, y_i, no)
+            x_hat_ls_i, no_ls_new_i = self.equalizer(h_hat_ls, y_i, no, x_data[:, i], 'ls')
             # print('x_hat_ls_i.shape: ', x_hat_ls_i.shape)
             # print('no_ls_new_i.shape: ', no_ls_new_i.shape)
             x_hat_ls.append(x_hat_ls_i)
@@ -95,7 +92,7 @@ class end2endModel(tf.keras.Model):
             
             # print('difference between x_hat_ls_i and x_data[:, i]: ', tf.reduce_sum(tf.abs(x_hat_ls_i - tf.reshape(x_data[:, i], [-1, 1]))))
 
-            x_hat_mmse_i, no_mmse_new_i = self.equalizer(h_hat_mmse, y_i, no)
+            x_hat_mmse_i, no_mmse_new_i = self.equalizer(h_hat_mmse, y_i, no, x_data[:, i], 'mmse')
             x_hat_mmse.append(x_hat_mmse_i)
             no_mmse_new.append(no_mmse_new_i)
                         

equalizers/equalizer.py
@@ -4,7 +4,7 @@ class equalizer(tf.keras.Model):
     def __init__(self):
         super().__init__()
         
-    def __call__(self, h_hat, y, no):
+    def __call__(self, h_hat, y, no, x, estimator):
                
         norm_h_hat_squared = tf.reduce_sum(tf.square(tf.abs(h_hat)), axis=-1)
                         
@@ -12,12 +12,22 @@ class equalizer(tf.keras.Model):
             no,
             norm_h_hat_squared
         )
+
+        # print('first 10 elements of y: ', y[0, 0, :])
+
+        # print('first 10 elements of h_hat^H: ', tf.transpose(h_hat, conjugate=True, perm=[0, 2, 1])[0, :, 0])
                         
-        inner_product_h_y = tf.reduce_sum(tf.matmul(y, tf.linalg.adjoint(h_hat)), axis=-1)
+        inner_product_h_y = tf.reduce_sum(tf.matmul(y, tf.transpose(h_hat, conjugate=True, perm=[0,2,1])), axis=-1)
                                                 
         x_hat = tf.math.divide_no_nan(
             inner_product_h_y,
             tf.cast(norm_h_hat_squared, dtype=tf.complex64)
         )
+
+        # if estimator == 'ls':
+        #     print('difference between x_hat_ls and x: ', tf.reduce_sum(tf.abs(x_hat - tf.reshape(x, [-1, 1]))))
+        # if estimator == 'mmse':
+        #     print('difference between x_hat_mmse and x: ', tf.reduce_sum(tf.abs(x_hat - tf.reshape(x, [-1, 1]))))
+
                         
         return x_hat, no_new

estimators/genie_mmse_estimator.py
@@ -17,37 +17,45 @@ class genie_mmse_estimator(tf.keras.Model):
         # print('check if scaled_C is hermitian', tf.reduce_all(tf.equal(scaled_C, tf.math.conj(tf.transpose(scaled_C, perm=[0, 2, 1])))))
         
         eigenvalues, eigenvectors = tf.linalg.eigh(scaled_C)
+
+        # print('shape of eigenvalues: ', eigenvalues.shape)
                         
         # Compute the inverse of (Lambda * noise_var)^-1
         inverse_lambda_noise_var = tf.linalg.inv(tf.linalg.diag(eigenvalues) + noise_var)
 
         # Compute the inverse of the sum
-        # inverse = tf.matmul(tf.matmul(eigenvectors, inverse_lambda_noise_var), tf.transpose(eigenvectors, conjugate=True, perm=[0, 2, 1]))
+        inverse = tf.matmul(tf.matmul(eigenvectors, inverse_lambda_noise_var), tf.transpose(eigenvectors, conjugate=True, perm=[0, 2, 1]))
         
         # print('shape of einsum of inverse_lambda_noise_var: ', tf.einsum('ijk,ilk->ijl', inverse_lambda_noise_var, tf.transpose(eigenvectors, conjugate=True, perm=[0, 1, 2])).shape)
         
-        # inverse = tf.einsum('ijk,ikl->ijl', eigenvectors, tf.einsum('ijk,ilk->ijl', inverse_lambda_noise_var, tf.transpose(eigenvectors, conjugate=True, perm=[0, 1, 2])))
+        # inverse_2 = tf.einsum('ijk,ikl->ijl', eigenvectors, tf.einsum('ijk,ilk->ijl', inverse_lambda_noise_var, tf.transpose(eigenvectors, conjugate=True, perm=[0, 1, 2])))
 
-        inverse = tf.linalg.inv(scaled_C + noise_var)
+        # inverse = tf.linalg.inv(scaled_C + noise_var)
         
         # print('where are inverse and inverse_2 the same: ', tf.reduce_sum(tf.where(tf.abs(inverse - inverse_2) > 1e-2, tf.ones_like(inverse), tf.zeros_like(inverse))))
         
+        # print('inverse * (noise + scaled_C): ', tf.matmul(inverse, noise_var + scaled_C)[33, :10, :10])
+
         # print('eigenvalues: ', eigenvalues[0, :])
-        # print('maximum value of eigenvalues: ', tf.math.reduce_max(tf.math.real(eigenvalues), axis=1)[0])
-        # print('minimum value of eigenvalues: ', tf.math.reduce_min(tf.math.real(eigenvalues), axis=1)[0])          
+        # print('shape of maximum value of eigenvalues: ', tf.math.reduce_max(tf.math.reduce_max(tf.math.real(eigenvalues), axis=1)))
+        # print('maximum value of eigenvalues: ', tf.math.reduce_max(tf.math.reduce_max(tf.math.real(eigenvalues), axis=1)))
+        # print('minimum value of eigenvalues: ', tf.math.reduce_min(tf.math.reduce_min(tf.math.real(eigenvalues), axis=1)))
+        # print('condition number of scaled_C: ', tf.math.reduce_sum(tf.math.reduce_max(tf.math.real(eigenvalues), axis=1) / (tf.math.reduce_min(tf.math.real(eigenvalues), axis=1))) / 500.0)
                 
         # scaled_C_2 = conj(p) * C
-        scaled_C_2 = tf.math.conj(pilot) * tf.cast(C, dtype=tf.complex64)                        
+        scaled_C_2 = tf.math.conj(pilot) * tf.cast(C, dtype=tf.complex64)
 
         # matrix = scaled_C_2 * inverse
         matrix = tf.matmul(scaled_C_2, inverse)
 
+        # print('first 10 elements of inverse: ', inverse[0, 0, :])
 
-
-                                                                                                                            
+        # print('first 10 elements of scaled_C_2: ', scaled_C_2[0, :, 0])
+        # print('first 10 elements of matrix: ', matrix[:5, 10:20, 10:20])
+                                                                                                                         
         # h_hat_mmse = (scaled_C_2 * inverse) * y. Be careful of the data types!
         h_hat_mmse = tf.matmul(matrix, tf.transpose(tf.cast(y, dtype=tf.complex64), perm=[0, 2, 1]))   
-        h_hat_mmse_2 = tf.einsum('ijk,ilk->ijl', matrix, y) 
+        # h_hat_mmse_2 = tf.einsum('ijk,ilk->ijl', matrix, y) 
 
         # print('shape of y: ', y.shape)
         # print('shape of h_hat_mmse: ', h_hat_mmse.shape)

model_3gpp/channel_generation.py
@@ -7,7 +7,7 @@ def channel_generation(batch_size, n_coherences, n_antennas):
     SIMO version.
     """
     path_sigma = 2.0
-    n_path = 1024
+    n_path = 1
     channel = SCMMulti(path_sigma=path_sigma, n_path=n_path)
 
     # generate channel samples with a certain batch size
@@ -20,7 +20,7 @@ def channel_generation(batch_size, n_coherences, n_antennas):
     
     for i in range(batch_size):
         # Calculate the covariance matrix for each sample
-        C[i, :, :] = toeplitz(t[i, :])
+        C[i, :, :] = np.transpose(toeplitz(t[i, :]))
     
     print('Generated ' + str(batch_size) + ' SIMO channel samples of size ' + str(n_antennas) + 'x1.')
     